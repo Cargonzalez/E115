e69b8f24513e40eddbe2d5eebd52a41ccb9466f6
Jocelyn Ragukonis <jragukon@stevens.edu>
2017-11-16 12:24:18 -0500

    fjahlsd

--- Test.cpp
+++ Test.cpp
@@ -6,6 +6,7 @@ int main () {
 	cout << "Hello there !  " << endl;
 	cout << "Hi!" << endl;
     cout << "What's up pal? " << endl;
+    cout << " COOL BEANS" << endl;
 
 	system("pause");
 	return 1;
